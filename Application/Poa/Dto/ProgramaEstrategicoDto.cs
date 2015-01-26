b2ea797d6f803b6699ef8de5fbcee353644a6beb
mdgaldamez <michaelgaldamez7@gmail.com>
2015-01-25 21:47:31 -0600

    Se agrego datannotations

--- Application/Poa/Dto/ProgramaEstrategicoDto.cs
+++ Application/Poa/Dto/ProgramaEstrategicoDto.cs
@@ -1,10 +1,17 @@
-﻿namespace Application.Poa.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Poa.Dto
 {
     public class ProgramaEstrategicoDto
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(35)]
         public string Nombre { get; set; }
+        [Required]
         public string Descripcion { get; set; }
+        [Required]
         public bool Activo { get; set; }
     }
 }
