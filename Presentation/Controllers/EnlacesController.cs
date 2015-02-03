89ebd0b14719cbbb747847b6368922425caa4bd0
Michael G <michaelgaldamez7@gmail.com>
2015-02-02 23:16:04 -0600

    Cambios en la capa de presentación

---
+++ Presentation/Controllers/EnlacesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class EnlacesController : Controller
+    {
+        //
+        // GET: /Enlaces/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Presentation/Controllers/MatricesDeSeguimientoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class MatricesDeSeguimientoController : Controller
+    {
+        //
+        // GET: /MatricesDeSeguimiento/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
